5f03ca16b7df877ec8e1fa0e0e913fd059144f06
sriman09 <39696623+sriman09@users.noreply.github.com>
2018-06-02 23:13:02 +0530

    logo changed

--- common/footer.php
+++ common/footer.php
@@ -3,7 +3,7 @@
                 <!-- top footer statrs -->
                 <div class="row top-footer">
                     <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
-                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                        <a href="#"> <img src="images/icon.png" height="90" width="94" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
                     <div class="col-xs-12 col-sm-3 about color-gray">
                         <h5>About Us</h5>
                         <ul>
