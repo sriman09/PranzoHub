d850f65ac00cb894b0b59af0d922db3e77161849
sriman09 <39696623+sriman09@users.noreply.github.com>
2018-06-02 23:07:47 +0530

    contact modified

--- contact.php
+++ contact.php
@@ -64,6 +64,8 @@
 
                 </div>
             </div>
+             <br>
+            <br>
             <section class="contact-page inner-page">
                 <div class="container">
                     <div class="row">
@@ -123,33 +125,21 @@
                     </div>
                 </div>
             </section>
-            <section class="app-section">
-                <div class="app-wrap">
-                    <div class="container">
-                        <div class="row text-img-block text-xs-left">
-                            <div class="container">
-                                <div class="col-xs-12 col-sm-6  right-image text-center hidden-xs-down">
-                                    <figure> <img src="images/app.png" alt="Right Image"> </figure>
-                                </div>
-                                <div class="col-xs-12 col-sm-6 left-text">
-                                    <h3>The Best Food Delivery App</h3>
-                                    <p>Now you can make food happen pretty much wherever you are thanks to the free easy-to-use Food Delivery &amp; Takeout App.</p>
-                                    <div class="social-btns">
-                                        <a href="#" class="app-btn apple-button clearfix">
-                                            <div class="pull-left"><i class="fa fa-apple"></i> </div>
-                                            <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">App Store</span> </div>
-                                        </a>
-                                        <a href="#" class="app-btn android-button clearfix">
-                                            <div class="pull-left"><i class="fa fa-android"></i> </div>
-                                            <div class="pull-right"> <span class="text">Available on the</span> <span class="text-2">Play store</span> </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+              <br>
+           <br>
+           <br>
+           <br>
+           <br>
+            <br>
+           <br>
+           <br>
+           <br>
+           <br>
+            <br>
+           <br>
+           <br>
+           <br>
+           <br>
             <!-- start: FOOTER -->
            <?php include 'common/footer.php'; ?>
             <!-- end:Footer -->
