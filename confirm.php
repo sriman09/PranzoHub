2d37c392670935b2ab1dc958d1fc8da3d655d340
sriman09 <39696623+sriman09@users.noreply.github.com>
2018-06-03 10:18:08 +0530

    confirm and failed
    2 new pages added confirm and failed

---
+++ confirm.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Confirmed!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<?php include 'common/navbar.php'; ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4">
+					<center><img alt="Bootstrap Image Preview" src="images/check.png" height="250" width="250" class="rounded-circle">
+					</center>
+					
+				</div>
+				<div class="col-md-4">
+				</div>
+			</div><br><br>
+			<div class="row">
+				<div class="col-md-12">
+					<center>
+					<h1 style="font-size: 80px" class="text-center">
+						<b>You're all set!</b>
+					</h1>
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<center>
+			<h3>
+				<span><b>Order Id #12345</b></span><br><br>
+			</h3>
+			<h3 style="font-size: 40px">
+				Thanks for being Awesomwe,<br>We hope you enjoy your Subscription!
+			</h3>
+
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+			</center>
+		</div>
+	</div>
+	<br><br><br><br><br>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ failed.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Oops!!!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<?php include 'common/navbar.php'; ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+				<div class="col-md-4">
+					<center><img alt="Bootstrap Image Preview" src="images/cancel.png" height="250" width="250" class="rounded-circle">
+					</center>
+					
+				</div>
+				<div class="col-md-4">
+				</div>
+			</div><br><br>
+			<div class="row">
+				<div class="col-md-12">
+					<center>
+					<h1 style="font-size: 80px" class="text-center">
+						<b>OOPS!<br>Something went wrong</b>
+					</h1>
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<center>
+			<!-- <h3>
+				<span><b>Order Id #12345</b></span><br><br>
+			</h3> --><br><br><br><br>
+			<h3 style="font-size: 40px">
+				Your order can't be compeleted because of an issue.
+			</h3>
+            <br><br>
+			<button type="button">Retry</button>
+			</center>
+		</div>
+	</div>
+	<br><br><br><br><br>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
